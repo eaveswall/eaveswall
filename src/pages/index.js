749ad3bbe80ff4ca4e7d7649a831d53957aec762
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-03-18 13:52:07 -0700

    our index/home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => {
-  return (
-    <div>Hello University of Ibadan!</div>
-  )
-}
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import AllPosts from "../components/all-posts"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" isHome />
+    <div className="d-flex flex-wrap justify-content-evenly mt-3 mb-5">
+      <AllPosts />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-3/">Go to page 3</Link>
+  </Layout>
+)
+
+export default IndexPage
