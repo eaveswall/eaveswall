3f2c06e8803fd35ea6151eeaacc8d793835f96e7
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-03-18 13:50:51 -0700

    Create gatsby-node.js
    This file contains middlewares for the gatsby compile time. Activities that should be run at build time, before build starts or after build ends, are put here

---
+++ gatsby-node.js
@@ -0,0 +1,54 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+const { createFilePath } = require("gatsby-source-filesystem")
+const path = require("path")
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === "Mdx") {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: "slug",
+      node,
+      value: `/posts${value}`,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions, repirter }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMdx {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild("ERROR: Loading `createPages` query")
+  }
+
+  const posts = result.data.allMdx.edges
+
+  posts.forEach(({ node }, index) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/components/post-layout.js`),
+      context: { id: node.id },
+    })
+  })
+}
