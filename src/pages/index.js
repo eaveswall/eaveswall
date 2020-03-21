4a6f9915f28efc47208d1adb4ebc9ffe4c984a24
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-03-20 23:23:55 -0700

    remove irrelevant links

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import AllPosts from "../components/all-posts"
+import Button from "../components/button"
 
 const IndexPage = () => (
   <Layout>
@@ -11,8 +12,6 @@ const IndexPage = () => (
     <div className="d-flex flex-wrap justify-content-evenly mt-3 mb-5">
       <AllPosts />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-    <Link to="/page-3/">Go to page 3</Link>
   </Layout>
 )
 
