a933ae9d4e72799382dc484f95b60a0e26b6868a
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-03-31 01:13:07 +0100

    default layout: remove FrontTile component

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,36 +20,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <FrontTile>
-        <div className="tile-text">
-          <div className="container ">
-            <div className="row">
-              <section
-                className="col-12 tile-lead m-auto"
-                style={{ fontSize: `1.3rem` }}
-              >
-                {data.site.siteMetadata.title}
-              </section>
-            </div>
-            <div className="row">
-              <section
-                className="col-12 tile-lead m-auto"
-                style={{ fontSize: `2.3rem`, fontWeight: 500 }}
-              >
-                The Eaves that Drop, The Wall that Falls
-              </section>
-            </div>
-            <div className="row">
-              <section className="col-4 tile-lead m-auto"></section>
-              <section className="col-8 tile-lead " style={{ fontSize: `1.2rem`, fontWeight: 500 }}>
-                {data.site.siteMetadata.description}
-              </section>
-            </div>
-          </div>
-          <div className="clippy"></div>
-        </div>
-      </FrontTile>
-      <Header siteTitle={data.site.siteMetadata.title} isHome />
+      <Header siteTitle={data.site.siteMetadata.title} shade />
       <div className="d-flex flex-column" style={{ minHeight: `calc(100vh - 102px)` }}>
         <div className="mt-5 mb-3 px-0 px-sm-3">
           <main>{children}</main>
