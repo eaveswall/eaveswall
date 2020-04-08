bee1fd551de40fd49609d05be2a31efadbb2cee8
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-04-08 19:12:31 +0100

    comment out unused imports

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import PageLayout from "../components/page-layout"
-import SEO from "../components/seo"
+// import SEO from "../components/seo"
 
 const About = () => (
   <>
