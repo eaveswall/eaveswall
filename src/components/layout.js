5131cf5c4c4c6d3791d5deaae044215d26244404
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-04-08 19:14:34 +0100

    comment out unused import

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import FrontTile from "../components/front-tiles-img"
+// import FrontTile from "../components/front-tiles-img"
 import Header from "./header"
 import Footer from "./footer"
 
