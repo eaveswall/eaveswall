bd3eda2f941708cb4bdd7b028ad6a993ddd6b061
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-04-14 22:08:44 +0100

    use styled-components to style button

--- src/components/button.js
+++ src/components/button.js
@@ -1,25 +1,37 @@
 import React from "react"
-
-import "./button.mod.scss"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
-const constructStylename = (type) => {
-  let style = " "
-  type.forEach(value => style += `button-${value} `)
-  return style
-}
+const Button = styled.button`
+  display: inline-block;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 500;
+  color: ${({color}) => color || 'purple'};
+  text-align: center;
+  white-space: normal;
+  vertical-align: middle;
+  cursor: pointer;
+  user-select: none;
+  background-color: transparent;
+  *border: 2px solid rgba(0,0,0,.1);
+  border-radius: 0.25rem;
+  padding: 0.4rem 1rem;
+  transition: all ease $trans-time;
+  background-color: #ffffff;
+  box-shadow: 0 0.2rem 0.28rem 0 rgba(0,0,0,.1);
+  &:hover {
+    color: currentColor;
+    text-decoration: none;
+    background-color: #f5f5f5;
+  }
+`
 
-const Button = ({ text, type=[], ...rest }) => (
-  <button styleName={`button${constructStylename(type)}`} {...rest}>{text}</button>
-  // <button styleName="button button-danger button-large">{text}</button>
+export const LinkButton = ({to, text, ...rest}) => (
+  <Button as={Link} to={to} {...rest}>{text}</Button>
 )
 
-export const LinkButton = ({to, text, type=[], ...rest}) => (
-  <Link to={to} styleName={`button${constructStylename(type)}link`} {...rest}>{text}</Link>
-)
-
-export const LinkButtonGeneric = ({to, text, type=[], ...rest}) => (
-  <a href={to} styleName={`button${constructStylename(type)}link`} {...rest}>{text}</a>
+export const LinkButtonGeneric = ({to, text, ...rest}) => (
+  <Button as="a" href={to} {...rest}>{text}</Button>
 )
 
 export default Button
