002cc614b669d2d0043240335200c4be7bb4d6b8
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-04-30 21:10:35 +0100

    themed: properly theme button

--- src/components/button.js
+++ src/components/button.js
@@ -5,24 +5,24 @@ import styled from "styled-components"
 const Button = styled.button`
   display: inline-block;
   font-family: 'Roboto', sans-serif;
+  font-size: 0.9rem;
   font-weight: 500;
-  color: ${({color}) => color || 'purple'};
+  color: ${({theme: {main}}) => main.fg};
   text-align: center;
   white-space: normal;
   vertical-align: middle;
   cursor: pointer;
   user-select: none;
   background-color: transparent;
-  *border: 2px solid rgba(0,0,0,.1);
   border-radius: 0.25rem;
   padding: 0.4rem 1rem;
-  transition: all ease $trans-time;
-  background-color: #ffffff;
-  box-shadow: 0 0.2rem 0.28rem 0 rgba(0,0,0,.1);
+  transition: all ease .4s;
+  background-color: ${({theme: {main}}) => main.bg};;
+  box-shadow: 0 0.2rem 0.728rem 0.2rem ${({theme: {main}}) => main.shade};;
   &:hover {
     color: currentColor;
     text-decoration: none;
-    background-color: #f5f5f5;
+    background-color: ${({theme: {main}}) => main.bgFair};;
   }
 `
 
