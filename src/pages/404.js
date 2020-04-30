41fdbd07670b14c87c63623178576fafc70d2ab1
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-04-30 21:19:06 +0100

    modified

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
     <div style={{ textAlign: `center` }}>
       <h1 style={{borderBottom: `5px solid #3d1928`,marginBottom: `1rem`}}><sub>4<sup>0<sup>4</sup></sup></sub> NOT FOUND <sup>4<sub>0<sub>4</sub></sub></sup></h1>
-      <p style={{fontSize: `1.2rem`}}>Oh, ooops! There is nothing to serve here. You just landed on a route that doesn't exists yet.</p>
+      <p style={{fontSize: `1.2rem`}}>Oh, ooops! There is nothing to serve here. You just landed on a route that doesn't exist, at least not yet.</p>
     </div>
   </PageLayout>
 )
