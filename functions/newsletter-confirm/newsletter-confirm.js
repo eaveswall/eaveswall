2b11d5199e26f422808b2e88661008191236b028
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-05-07 16:11:46 +0100

    cloud function to do newsletter sub. confirmation

---
+++ functions/newsletter-confirm/newsletter-confirm.js
@@ -0,0 +1,24 @@
+const hamSubmissionState = require("./assets/mark-as-ham")
+
+const FORM_ID = "5eb03b64ed85550007242fba"
+
+exports.handler = (event, _ctx, callback) => {
+  const req = JSON.parse(event)
+  const { id, fid } = req.queryStringParameters
+
+  if (fid !== FORM_ID) {
+    callback("Error data mismatch")
+  }
+
+  hamSubmissionState(id).then((status) => {
+    console.log("STATUS: ", status)
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({success: true}),
+    })
+  }).catch(err =>
+    console.log("Error setting submission as spam: ", err)
+  )
+
+
+}
