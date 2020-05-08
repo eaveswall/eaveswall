58bf88ecd1fec5e9281ba79ec96eb1ccddfd9dc4
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-05-08 14:23:38 +0100

    debugging

--- functions/subscribe/subscribe.js
+++ functions/subscribe/subscribe.js
@@ -9,44 +9,69 @@ const FUNCTIONS_ENDPOINT = ".netlify/functions"
 const NEWSLETTER = "newsletter"
 const NEWSLETTER_CONFIRM_MSG = "./assets/confirm-newsletter.ejs"
 
+const generateId = () => {
+  const crypto = require("crypto")
+  return crypto.randomBytes(16).toString("hex")
+}
+
+const encode = data => {
+  return Object.keys(data)
+    .map(key => {
+      return `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+    })
+    .join("&")
+}
+
+const netlifySubmit = async data => {
+  const res = await fetch(BASE_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: encode(data),
+  })
+  return await res.json()
+}
+
 const handleNewsletter = payload => {
-  const [email, id, fid] = [
-    payload.data.email,
-    payload.id,
-    payload.form_id,
+  const [email, id, form_name] = [
+    payload.email,
+    generateId(),
+    payload["form-name"],
   ].map(value => encodeURIComponent(value))
 
-  const confirmLink = `${BASE_URL}/${FUNCTIONS_ENDPOINT}/newsletter-confirm?id=${id}&fid=${fid}`
-  const messageFile = require.resolve(NEWSLETTER_CONFIRM_MSG)
+  netlifySubmit({ "form-name": form_name, email, id })
+    .then(data => console.log(data))
+    .catch(console.error)
 
-  existsInSpam(id).then(yes => {
-    console.log("exists in spam: ", yes)
-    if (!yes) {
-      ejs.renderFile(messageFile, { data: { confirmLink } }).then(message => {
-        sendMail({
-          from: "Eaveswall Team <team@eaveswall.com>",
-          to: decodeURIComponent(email),
-          subject: "Newsletter Confirmation",
-          html: message,
-        })
-          .then(info => {
-            console.log("Confirmation email sent successfully", info)
-          })
-          .catch(err => console.log("Failed to send email", err))
-      })
+  // const confirmLink = `${BASE_URL}/${FUNCTIONS_ENDPOINT}/newsletter-confirm?id=${id}&fid=${fid}`
+  // const messageFile = require.resolve(NEWSLETTER_CONFIRM_MSG)
 
-      spamSubmissionState(id)
-        .then(status => console.log("Submission added to spam ", status))
-        .catch(err => console.log("Error setting submission as spam", err))
-    }
-  })
+  // existsInSpam(id).then(yes => {
+  //   console.log("exists in spam: ", yes)
+  //   if (!yes) {
+  //     ejs.renderFile(messageFile, { data: { confirmLink } }).then(message => {
+  //       sendMail({
+  //         from: "Eaveswall Team <team@eaveswall.com>",
+  //         to: decodeURIComponent(email),
+  //         subject: "Newsletter Confirmation",
+  //         html: message,
+  //       })
+  //         .then(info => {
+  //           console.log("Confirmation email sent successfully", info)
+  //         })
+  //         .catch(err => console.log("Failed to send email", err))
+  //     })
+
+  //     spamSubmissionState(id)
+  //       .then(status => console.log("Submission added to spam ", status))
+  //       .catch(err => console.log("Error setting submission as spam", err))
+  //   }
+  // })
 }
 
 exports.handler = event => {
   console.log(event)
-  const payload = JSON.parse(event.body).payload
-  console.log(payload)
-  if (payload.form_name === NEWSLETTER || event.httpMethod === POST) {
+  const payload = querystring.parse(event.body)
+  if (payload["form-name"] === NEWSLETTER || event.httpMethod === POST) {
     handleNewsletter(payload)
   }
 }
