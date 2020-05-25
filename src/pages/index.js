3ff124ff8dca35d13868df3961fd9036a1e5fb53
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-05-25 22:33:51 +0100

    social and visual enhancement

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,30 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layouts"
 import SEO from "../components/seo"
 import AllPosts from "../components/all-posts"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Eaveswall" isHome />
-    <div className="d-flex flex-wrap justify-content-evenly mt-3 mb-5">
-      <AllPosts />
-    </div>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      icon: file(relativePath: { eq: "eaveswall-icon.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <SEO title="Eaveswall" image={data.icon.childImageSharp.fluid.src} isHome />
+      <div className="d-flex flex-wrap justify-content-evenly mt-3 mb-5">
+        <AllPosts />
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
