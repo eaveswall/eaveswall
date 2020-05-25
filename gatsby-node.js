6feab2d6b57ca060bfaa11ce524a7b0b27eb02f2
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-05-25 22:09:26 +0100

    build some few other pages programatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,6 +8,7 @@
 
 const { createFilePath } = require("gatsby-source-filesystem")
 const path = require("path")
+const _ = require("lodash")
 
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   if (stage === "build-html") {
@@ -44,13 +45,29 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
   const result = await graphql(`
     query {
-      allMdx {
+      allPosts: allMdx(filter: { frontmatter: { publish: { eq: true } } }) {
         edges {
           node {
             id
             fields {
               slug
             }
+          }
+        }
+      }
+      tagsGroup: allMdx(
+        limit: 2000
+        filter: { frontmatter: { publish: { eq: true } } }
+      ) {
+        group(field: frontmatter___tags) {
+          fieldValue
+        }
+      }
+      allAuthorsJson {
+        edges {
+          node {
+            id
+            name
           }
         }
       }
@@ -61,12 +78,34 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     reporter.panicOnBuild("ERROR: Loading `createPages` query")
   }
 
-  const posts = result.data.allMdx.edges
+  const posts = result.data.allPosts.edges
 
   posts.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
       component: path.resolve(`./src/components/layouts/post-layout/index.js`),
+      context: { id: node.id },
+    })
+  })
+
+  const tags = result.data.tagsGroup.group
+
+  tags.forEach(tag => {
+    createPage({
+      path: `/tags/${_.kebabCase(tag.fieldValue)}/`,
+      component: path.resolve(`./src/components/layouts/tags-layout/index.js`),
+      context: { tag: tag.fieldValue },
+    })
+  })
+
+  const authors = result.data.allAuthorsJson.edges
+
+  authors.forEach(({ node }) => {
+    createPage({
+      path: `/authors/${_.kebabCase(node.name)}/`,
+      component: path.resolve(
+        `./src/components/layouts/authors-layout/index.js`
+      ),
       context: { id: node.id },
     })
   })
