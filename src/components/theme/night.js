8eaec02a039f1440570982b58b7fa9d87a392d4c
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-05-27 10:59:38 +0100

    adjust color palette

--- src/components/theme/night.js
+++ src/components/theme/night.js
@@ -1,13 +1,13 @@
 export default {
   bg: `#000000`,
-  bgAlt: `#2f2f2f`,
-  bgCold: `#1f1f1f`,
-  bgFair: `#0f0f0f`,
+  bgAlt: `#0f0f0f`,
+  bgCold: `#2f2f2f`,
+  bgFair: `#1f1f1f`,
   fg: `#ffffff`,
   fgAlt: `#cccccc`,
   fgCold: `#f1f1f1`,
   fgFair: `#eeeeee`,
-  shade: `rgba(255,255,255,.3)`,
+  shade: `rgba(255,255,255,.1)`,
   shadeAlt: `rgba(0,0,0,.1)`,
   day: false,
   night: true
