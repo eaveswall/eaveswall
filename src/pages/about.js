b39c0b66eb64a9ddd55c60ac5505398ede33e9c9
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-05-27 15:36:52 +0100

    updated

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,34 @@
 import React from "react"
 
 import PageLayout from "../components/layouts/page-layout"
-// import SEO from "../components/seo"
+import StyledTitle from "../components/title"
+import { Link } from "gatsby"
+import SEO from "../components/seo"
 
 const About = () => (
   <>
-    <PageLayout>
-      <p style={{textAlign: `center`, fontSize: `1rem`}}>We want to tell you about this beautiful site. What do you think we gon' say? Well we got nothin' to say. You already know what I'm going to say: "The eaves that drop; the wall that falls."</p>
+    <PageLayout activeNav={4}>
+      <SEO title="About" />
+      <div
+        style={{ margin: `3rem 0`, fontFamily: ``, fontSize: `1rem` }}
+      >
+        <StyledTitle>About</StyledTitle>
+        <p>
+          Eaveswall is a campus blog and not your "normal gossip sites". We just
+          want to bring the fun to all, without restrictions, 'cause
+          there is no fun in restrictions. We are not a{" "}
+          <em>"See what Davido ate last night"</em>&nbsp;&nbsp;kinda blog or a{" "}
+          <em>"You won't believe..."</em>&nbsp;&nbsp;kinda...those are just
+          click baits you often times fall for while you end up not being
+          amused. We are more like <Link>go find out for yourself</Link>.
+          Eaveswall is a place to make fun, share opinions, in fact do whatever you want freely.
+          {/* eslint-disable-next-line */}
+          <span role="presentation">😘</span>
+        </p>
+        <p>
+          The Eaveswall ecosystem includes realtime interactions on twitter, whatsapp and  instagram. Promotions, giveaways and contests are part of what makes the ecosystem we create a great one, too.
+        </p>
+      </div>
     </PageLayout>
   </>
 )
