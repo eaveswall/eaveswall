5a11a1df2715106835b2a16ccbab076c7618bcbd
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-05-29 22:57:20 +0100

    remove unused def
    But wait the whole module is unused 🤷

--- src/components/image.js
+++ src/components/image.js
@@ -13,7 +13,7 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = ({ name }) => {
+const Image = () => {
   const data = useStaticQuery(graphql`
     query {
       placeholderImage: file(relativePath: { eq: "sound.jpg" }) {
