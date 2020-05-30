0444123e694fc288893e4dfed5853424017e8abe
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-05-30 18:43:06 +0100

    WIP: add promotion pages

---
+++ src/pages/promotions/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+// import PropTypes from "prop-types"
+// import { graphql, Link } from "gatsby"
+import PageLayout from "../../components/layouts/page-layout"
+import { LinkButtonGeneric } from "../../components/button"
+
+import SEO from "../../components/seo"
+
+const Promotions = () => {
+  return (
+    <PageLayout>
+      <SEO title="Advertise" />
+      <div className="mb-3">Want to advertise on Eaveswall?</div>
+      <LinkButtonGeneric href="https://wa.me/2347066029547">
+        Contact Caleb
+      </LinkButtonGeneric>
+    </PageLayout>
+  )
+}
+
+export default Promotions
