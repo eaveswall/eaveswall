642e7a72c12fbea7352a51494002364bb7e4399c
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-05-30 01:38:48 +0100

    fix error pre-rendering html due to inavail. of `SiteTheme` in/and gatsby-ssr.js

--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -2,6 +2,7 @@ import React from "react"
 import { SiteTheme } from "./theme"
 
 const BlogWrapper = ({ children }) => {
+  // implement some stuff like site-wide alert here
   return <SiteTheme>{children}</SiteTheme>
 }
 
