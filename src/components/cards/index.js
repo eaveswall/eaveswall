9e0fccd6dfc7ea6cab69e7c5be8b0c37d2b8fcd6
Caleb Adepitan <calebpitan@outlook.com>
2020-08-18 10:20:29 +0100

    Add ellipsis to long author names

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -7,30 +7,40 @@ import StyledCard, {
   StyledCardContent,
 } from "./card"
 
-const Card = ({ image, title, mute, content }) => (
-  <StyledCard>
-    <StyledCardMeta>
-      <StyledCardImage
-        style={{
-          position: `relative`,
-          backgroundImage: `url(${image.src}),
+const Card = ({ image, title, mute, content }) => {
+  const maxAuthorNameLength = 15
+  let [author, date] = mute.split(/\u2014/, 2)
+  author = author.trim()
+
+  if (author.length > maxAuthorNameLength) {
+    author = author.substring(0, maxAuthorNameLength) + '...'
+  }
+
+  return (
+    <StyledCard>
+      <StyledCardMeta>
+        <StyledCardImage
+          style={{
+            position: `relative`,
+            backgroundImage: `url(${image.src}),
             url(${image.base64})`,
-        }}
-      />
-      <div className="px-3 py-2">
-        <h6 className="text-truncate" style={{ margin: 0, fontWeight: 900 }}>
-          {title}
-        </h6>
-        <small className="text-muted">
-          <strong>{mute}</strong>
-        </small>
-      </div>
-    </StyledCardMeta>
-    <StyledCardContent>
-      <div className="content p-3">{content}</div>
-    </StyledCardContent>
-  </StyledCard>
-)
+          }}
+        />
+        <div className="px-3 py-2">
+          <h6 className="text-truncate" style={{ margin: 0, fontWeight: 900 }}>
+            {title}
+          </h6>
+          <small className="text-muted">
+            <strong>{author} &mdash; {date}</strong>
+          </small>
+        </div>
+      </StyledCardMeta>
+      <StyledCardContent>
+        <div className="content p-3">{content}</div>
+      </StyledCardContent>
+    </StyledCard>
+  )
+}
 
 Card.propTypes = {
   title: PropTypes.string,
